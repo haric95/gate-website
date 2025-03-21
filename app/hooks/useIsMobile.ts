2534acce8e63f4e5c8b94bd3c50d580399e04415
Hari Chauhan <hari-c@hotmail.co.uk>
2025-03-21 19:47:28 +0000

    Homepage live

--- app/hooks/useIsMobile.ts
+++ app/hooks/useIsMobile.ts
@@ -3,7 +3,7 @@ import { useCallback, useEffect, useState } from "react";
 const BREAKPOINT = 768;
 
 export const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
 
   const callback = useCallback(() => {
     if (window.innerWidth <= BREAKPOINT) {
--- app/page.tsx
+++ app/page.tsx
@@ -27,43 +27,141 @@ export default function Home() {
   const { isMobile } = useIsMobile();
   return (
     <main
-      className="flex flex-col items-center justify-center p-16"
+      className="flex flex-col items-center justify-center p-4 md:p-16"
       style={{ minHeight: "calc(100vh - 144px)" }}
     >
       <div className="flex flex-col items-center w-full">
-        {isMobile ? (
-          // Mobile
-          <>
-            <div className="flex">
-              <h1 className="text-[20px] title textoutline green text-center">
-                <WordRepeat>
-                  <LetterPositioning>GATE</LetterPositioning>
-                </WordRepeat>
-              </h1>
-              <h1 className="text-[20px] title textoutline green text-center">
-                <WordRepeat>
-                  <LetterPositioning>@</LetterPositioning>
-                </WordRepeat>
-              </h1>
-              <h1 className="text-[20px] mb-4 title textoutline green text-center">
-                <WordRepeat>
-                  <LetterPositioning>The Distillery N17</LetterPositioning>
-                </WordRepeat>
-              </h1>
-              <h2 className="text-[16px] mb-16 title textoutline blue text-center">
-                <WordRepeat>
-                  <LetterPositioning>18 - 04 - 25</LetterPositioning>
-                </WordRepeat>
-              </h2>
+        {isMobile === null ? null : isMobile ? (
+          <div className="w-full">
+            <div className="flex flex-col">
+              <div className="flex flex-col items-start justify-center w-full">
+                <h1 className="text-[20px] mb-4 title textoutline green">
+                  <WordRepeat>
+                    <LetterPositioning>
+                      GATE @ The Distillery N17
+                    </LetterPositioning>
+                  </WordRepeat>
+                </h1>
+                <h2 className="text-[20px] mb-4 title textoutline blue">
+                  <WordRepeat>
+                    <LetterPositioning>18 APR 25</LetterPositioning>
+                  </WordRepeat>
+                </h2>
+                <h2 className="text-[20px] mb-8 title textoutline red">
+                  <WordRepeat>
+                    <LetterPositioning>4PM - 4AM</LetterPositioning>
+                  </WordRepeat>
+                </h2>
+                <div className="text-[16px] textshadow mb-8">
+                  <p className="h-[144px]">
+                    <TypeAnimation
+                      sequence={[
+                        "Recall GATE\ndaughters of the forgotten zones\nflung scattered to the wind\nacross seas and rivers\nto reconvene in\nsha(ttere)red solitude",
+                      ]}
+                      wrapper="span"
+                      speed={60}
+                      style={{
+                        display: "inline-block",
+                        whiteSpace: "pre-line",
+                      }}
+                    />
+                  </p>
+                </div>
+                <div className="mb-16">
+                  <div className="flex items-center mb-4">
+                    <Link href="/tickets">
+                      <button className="w-full flex items-center justify-center inputoutline text-white p-4 w-64 mr-8 text-sm">
+                        BUY TICKETS
+                      </button>
+                    </Link>
+                    <h2 className="text-[24px] title textoutline text-white text-center ml-4">
+                      <WordRepeat>
+                        <LetterPositioning>£12</LetterPositioning>
+                      </WordRepeat>
+                    </h2>
+                  </div>
+                  <p className="text-sm">
+                    No one will be turned away for lack of funds!
+                  </p>
+                  <p className="text-sm w-2/3">
+                    Please contact GATE through Instagram or at
+                    gatheralltheelectronics@gmail.com and we will sort you out.
+                  </p>
+                </div>
+                <img src="/posters/1.jpg" className="mb-16" />
+                <h2 className="text-[32px] mb-4 title textoutline green text-center">
+                  <WordRepeat>
+                    <LetterPositioning>LINEUP</LetterPositioning>
+                  </WordRepeat>
+                </h2>
+                <div>
+                  <div className="mb-8 text-xl">
+                    <p>LIVE ELECTRONIC MUSIC ///</p>
+                    <p>Jena Jang</p>
+                    <p>Gus Cantor</p>
+                    <p>Jack Dove</p>
+                    <p>Mi yá</p>
+                    <p>Cold Light</p>
+                    <div className="flex">
+                      <p>Seshtan</p>
+                      <img src="/logo.png" className="inline h-[32px]" />
+                    </div>
+                    <div className="flex">
+                      <p>Internal Object + Silkenste</p>
+                      <img src="/logo.png" className="inline h-[32px]" />
+                    </div>
+                    <div className="flex">
+                      <p>Lissener</p>
+                      <img src="/logo.png" className="inline h-[32px]" />
+                    </div>
+                  </div>
+                  <div className="mb-8 text-xl">
+                    <p>DJ /////////////////////</p>
+                    <p>Proc Fiskal</p>
+                    <p>Demetzy</p>
+                    <p>Medallion Man</p>
+                    <div className="flex">
+                      <p>Overneath</p>
+                      <img src="/logo.png" className="inline h-[32px]" />
+                    </div>
+                  </div>
+                  <div className="mb-8 text-xl">
+                    <p>INSTALLATIONS ////////////</p>
+                    <p>Tabitha Weddell</p>
+                    <p>Baos</p>
+                    <div className="flex">
+                      <p>Martha</p>
+                      <img src="/logo.png" className="inline h-[32px]" />
+                    </div>
+                  </div>
+                  <div className="mb-8 text-xl">
+                    <p>PROJECTIONS //////////////</p>
+                    <p>Visuals Ru</p>
+                  </div>
+                  <p className="text-lg mb-8">
+                    Home cooked vegan food will be available from 4-8pm.
+                  </p>
+                  <p className="text-lg">//////////////////////////</p>
+                </div>
+              </div>
+              <div className="flex items-center mt-8">
+                <Link href="/tickets">
+                  <button className="w-full flex items-center justify-center inputoutline text-white p-4 w-64 mr-8 text-sm">
+                    BUY TICKETS
+                  </button>
+                </Link>
+                <h2 className="text-[24px] title textoutline text-white text-center ml-4">
+                  <WordRepeat>
+                    <LetterPositioning>£12</LetterPositioning>
+                  </WordRepeat>
+                </h2>
+              </div>
             </div>
-            <div>
-              <EmailForm />
-            </div>
-          </>
+          </div>
         ) : (
           /* Desktop */
           <div className="flex w-full">
-            <div className="flex flex-col w-1/2">
+            <div className="flex flex-col w-1/2 mr-8">
               <div className="flex flex-col items-start justify-center w-full">
                 <h1 className="md:text-[32px] text-[24px] mb-4 title textoutline green">
                   <WordRepeat>
@@ -176,7 +274,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="flex w-1/2">
+            <div className="w-1/2">
               <img src="/posters/1.jpg" />
             </div>
           </div>
